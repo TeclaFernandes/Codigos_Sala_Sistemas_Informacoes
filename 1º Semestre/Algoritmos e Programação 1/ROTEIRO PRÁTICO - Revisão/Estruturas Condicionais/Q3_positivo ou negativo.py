32aa575611fab7899cf40f68ef81007ee2c4d8b5
Tecla Fernandes Oliveira <115437392+TeclaFernandes@users.noreply.github.com>
2026-06-24 23:47:27 -0300

    feat: positivo, negativo ou zero
    Algoritmos e Programação 1

---
+++ 1º Semestre/Algoritmos e Programação 1/ROTEIRO PRÁTICO - Revisão/Estruturas Condicionais/Q3_positivo ou negativo.py
@@ -0,0 +1,8 @@
+num = int(input("Informe um numero para verificação: "))
+
+if num == 0:
+    print('O numero informado é ZERO!')
+elif num > 0:
+    print('O numero informado é POSITIVO!')
+elif num < 0:
+    print('O numero informado é NEGATIVO!')
