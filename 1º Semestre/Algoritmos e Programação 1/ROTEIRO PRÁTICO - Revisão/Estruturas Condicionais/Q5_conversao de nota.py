78dee50d529d628c397f7fc2b68fca9e69c779d0
Tecla Fernandes Oliveira <115437392+TeclaFernandes@users.noreply.github.com>
2026-06-24 23:49:24 -0300

    feat: conversão de numerica para A, B, C, D ou E
    Algoritmos e Programação 1

---
+++ 1º Semestre/Algoritmos e Programação 1/ROTEIRO PRÁTICO - Revisão/Estruturas Condicionais/Q5_conversao de nota.py
@@ -0,0 +1,10 @@
+nota = float(input("Informe a nota para converção: "))
+
+if nota >= 0 and nota < 4:
+    print('E')
+if nota >= 4 and nota < 6: 
+    print('C')
+if nota >= 6 and nota < 8:
+    print('B')
+if nota >= 8 and nota < 10:
+    print('A')
