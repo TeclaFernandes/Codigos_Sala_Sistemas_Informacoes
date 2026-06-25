896ba099a85bb0acaa4fb133cbf2d7426648cd36
Tecla Fernandes Oliveira <115437392+TeclaFernandes@users.noreply.github.com>
2026-06-24 23:55:58 -0300

    feat: tabuada
    Algoritmos e Programação 1

---
+++ 1º Semestre/Algoritmos e Programação 1/ROTEIRO PRÁTICO - Revisão/Estruturas de Repetição/Q2_tabuada.py
@@ -0,0 +1,6 @@
+n = int(input("Informe um numero: "))
+for i in range(1, 10 + 1):
+    print(f'{n} + {i} = {n+i}')
+    print(f'{n} - {i} = {n-i}')
+    print(f'{n} * {i} = {n*i}')
+    print(f'{n} / {i} = {n/i}')
